533f2fb18b44771f129df20cd6e199187045fc9e
Kate Flood <katefloodster@gmail.com>
2014-04-04 11:52:16 -0700

    initial rails framework added

---
+++ garden_exchange/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GardenExchange::Application.config.secret_key_base = 'a41dfec4b778d7485dfe27339f53a2d2d2a66cf8ca0d97f4155c8d8fc19b2dd827d418c5ac338e0a68e1d3391bb57b0c26de6492a7fc8a029d30910ab911c691'
---
+++ garden_exchange/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GardenExchange::Application.config.session_store :cookie_store, key: '_garden_exchange_session'
