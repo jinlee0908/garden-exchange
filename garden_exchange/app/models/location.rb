3f5942bda9de6d0979bc00d7911a0226248cbde5
JinLee <jinlee0908@gmail.com>
2014-04-10 18:47:44 -0700

    route path tests correction

--- garden_exchange/app/models/location.rb
+++ garden_exchange/app/models/location.rb
@@ -1,6 +1,4 @@
 class Location < ActiveRecord::Base
-
-  attr_accessible :address, :latitude, :longitude
   geocoded_by :address #this can be a method to pull in... 
   after_validation :geocode, :if => :address_changed?
 
--- garden_exchange/spec/requests/static_pages_spec.rb
+++ garden_exchange/spec/requests/static_pages_spec.rb
@@ -5,12 +5,12 @@ describe "StaticPages" do
   describe "Home Page" do
 
     it "should have the content 'Welcome to Garden Exchange'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Garden Exchange')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page). to have_title('Garden Exchange | Home')
     end
   end
@@ -18,12 +18,12 @@ describe "StaticPages" do
   describe "Search List Page" do
 
     it "should have the content 'Search List' " do
-      visit '/static_pages/search_list'
+      visit search_list_path
       expect(page).to have_content('Search List')
     end
 
     it "should have the title 'Search List'" do
-      visit '/static_pages/search_list'
+      visit search_list_path
       expect(page). to have_title('Garden Exchange | Search List')
     end
   end
