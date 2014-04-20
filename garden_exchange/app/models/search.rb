cad8c60a50443a890b6cf8256e7e5e0cd9033a44
JinLee <jinlee0908@gmail.com>
2014-04-19 20:28:00 -0700

    add infowindow

--- garden_exchange/app/models/search.rb
+++ garden_exchange/app/models/search.rb
@@ -6,6 +6,8 @@ class Search
    Gmaps4rails.build_markers(items) do |item, marker|
       marker.lat item.latitude
       marker.lng item.longitude
+      marker.title item.category.category
+      marker.infowindow "<b>#{item.name}</b>"
     end
   end
 
